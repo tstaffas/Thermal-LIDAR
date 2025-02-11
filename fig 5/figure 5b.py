e09689170cb2e86599e4c60c949680109d92ba73
Theodor Staffas <37420485+tstaffas@users.noreply.github.com>
2025-02-11 11:54:58 +0100

    Add files via upload

--- fig 5/figure 5b.py
+++ fig 5/figure 5b.py
@@ -26,51 +26,6 @@ def dist_to_time(d):
     t = d*1.462/c*1e9
     return t
 
-# ------- ETA analysis ----------
-def load_eta(recipe, **kwargs):
-    print('Loading ETA')
-    with open(recipe, 'r') as filehandle:
-        recipe_obj = json.load(filehandle)
-
-    eta_engine = eta.ETA()
-    eta_engine.load_recipe(recipe_obj)
-
-    # Set parameters in the recipe
-    for arg in kwargs:
-        eta_engine.recipe.set_parameter(arg, str(kwargs[arg]))
-
-    eta_engine.load_recipe()
-
-    return eta_engine
-
-def eta_analysis(file, eta_engine):
-    print('Starting ETA analysis')
-    print(f'{file=}')
-    cut = eta_engine.clips(Path(file), format=1)
-    result = eta_engine.run({"timetagger1": cut}, group='quTAG')
-    print('Finished ETA analysis')
-
-    return result
-
-def second_correleation(timetag_file, eta_engine, bins, binsisize):
-    # ETA analys
-    result = eta_analysis(timetag_file, eta_engine)
-
-    # extract result
-    hist1 = result["h3"]
-    hist2 = result["h4"]
-    hist0 = result["h4_zero"]
-    hist1[0] += hist0[0]
-
-    coin = np.concatenate((hist2[::-1], hist1))
-
-    delta_t = np.arange(-bins, bins) * binsize * 1e-3
-
-    g2 = coin / np.mean(coin)
-
-    print(f'Peak coincidence: {np.max(coin)} \nTotal detected photon pairs: {np.sum(coin)}\n Noise lvl: {np.mean(coin)}\n')
-
-    return coin, g2, delta_t
 
 
 """
@@ -87,26 +42,15 @@ ticksize = 12
 hfont = 'Arial'
 
 
-#Data analysis
-eta_recipe = 'Correlation-swabian.eta'
-
 #Data file
-timetag_file5 = 'kista_13.5km_10km_10km_delay_det2_16.35uA_det1_13uA_30s_241030.timeres'
+data_file = 'kista_13.5km_10km_10km_delay_det2_16.35uA_det1_13uA_30s_241030.txt'
 
-#ETA Settings
-binsize = 20
-bins = 350000
-delay = int(1.95e8-2.8e6-0.7e3) + int(132.0e6) + int(99.8841e6 - 55.82e3 ) 
-
-
-time_axis = np.arange(0,bins)*binsize
-eta_engine = load_eta(eta_recipe, bins=bins, binsize=binsize, delay_1 = delay)
-coin, g2, delta_t = second_correleation(timetag_file5, eta_engine,bins, binsize)
-
+#measurement Settings
+delta_t, coin = np.loadtxt(data_file, unpack = True)
+g2 = coin/np.mean(coin)
 
 # ---- Plot the results ----
-long_time = delta_t/1e3 + delay/1e6
-ax2.plot(long_time, g2, c = '#58508d', label = 'Deployed fiber')
+ax2.plot(delta_t/1e6, g2, c = '#58508d', label = 'Deployed fiber')
 
 ax2.legend(loc='upper left')
 ax2.set_xlabel('Time [$\mu$s]', fontsize = labelsize, fontname = hfont)
@@ -132,7 +76,7 @@ ax2t = ax2.secondary_xaxis('top', functions=(time_to_dist, dist_to_time))
 
 #---- Make a zoomed in plot ----
 ax2_zoom = ax2.inset_axes([0.4, 0.4, 0.5, 0.55])
-ax2_zoom.plot((delta_t+delay/1000)/1000, g2, c = '#58508d')
+ax2_zoom.plot((delta_t)/1e6, g2, c = '#58508d')
 ax2_zoom.set_xlim(424.022, 424.032 )
 ax2_zoom.set_ylim(0.9, 1.55)
 ax2_zoom.tick_params(left = True, right = False , labelleft = True ,labelbottom = True, bottom = True)
